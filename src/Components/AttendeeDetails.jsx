1805ae3c1ef6c642d2d822dc0083e5bec19fe87b
Tobiloba Olajiga <tobi@tm30.dev>
2025-02-13 17:05:52 +0100

    commit

--- src/Components/AttendeeDetails.jsx
+++ src/Components/AttendeeDetails.jsx
@@ -10,7 +10,7 @@ export default function AttendeeDetails({
   return (
     <div>
       {attendeeDetails && (
-        <div className=" h-full border border-[#0e464f] bg-[#041e23] min-w-[335px] sm:w-[700px] rounded-3xl p-8 mb-8">
+        <div className=" h-full border border-[#0e464f] bg-[#041e23] max-w-[335px] w-[335px] sm:w-[700px] rounded-3xl p-8 mb-8 mx-6 sm:mx-0">
           <Header title="Attendee Details" step="2" />
           <Form showTicket={showTicket} goBack={goBack} />
         </div>
--- src/Components/Ticket.jsx
+++ src/Components/Ticket.jsx
@@ -5,7 +5,7 @@ export default function Ticket({ ticket, showTicketSelect }) {
   return (
     <div>
       {ticket && (
-        <div className="h-full border border-[#0e464f] rounded-3xl sm:p-8 py-4 min-w-[335px] sm:w-[700px]  mb-8 bg-[#041E23] px-4">
+        <div className="h-full border border-[#0e464f] rounded-3xl sm:p-8 py-4 max-w-[335px] sm:max-w-[700px]  mb-8 bg-[#041E23] px-4">
           <Header title="Ready" step="3" />
           <PrintTicket showTicketSelect={showTicketSelect} />
         </div>
