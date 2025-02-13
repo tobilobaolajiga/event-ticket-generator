43a1a3e18b4604e76253bc3f4ac82ef5884d853c
Tobiloba Olajiga <tobi@tm30.dev>
2025-02-13 14:44:09 +0100

    commit

--- src/Components/TicketSelection.jsx
+++ src/Components/TicketSelection.jsx
@@ -1,4 +1,3 @@
-
 import TicketType from './TicketType';
 
 export default function TicketSelection({
@@ -11,28 +10,28 @@ export default function TicketSelection({
 }) {
   return (
     <div>
-      <div className="sm:border sm:border-[#0e464f] rounded-2xl px-6 my-8 xs:min-w-[300px] sm:w-[650px] xs:-ml-4 sm:ml-0 bg-[#08252b">
+      <div className="sm:border sm:border-[#0e464f] rounded-2xl my-8 min-w-[300px] sm:w-[650px] -ml-4 sm:ml-0 bg-[#08252b sm:px-6">
         <div
-          className="flex items-center flex-col justify-center gap-2 border border-[#07373F] py-4 rounded-2xl my-4 backdrop-blur-[7px]"
+          className="flex items-center flex-col justify-center gap-2 border border-[#07373F] py-4 rounded-2xl my-4 backdrop-blur-[7px] min-w-[280px] "
           style={{
             backgroundImage:
               'radial-gradient(52.52% 32.71% at 50% 97.66%, rgba(36, 160, 181, 0.2) 0%, rgba(36, 160, 181, 0) 100%)',
           }}
         >
-          <h2 className="xs:text-[46px] sm:text-[62px] font-road -mb-4">
+          <h2 className="text-[54px] sm:text-[62px] font-road -mb-4">
             Techember Fest '25
           </h2>
-          <p className="font-roboto xs:text-[14px] sm:text-[16px] text-center mx-4 sm:w-[350px]">
+          <p className="font-roboto text-[14px] sm:text-[16px] text-center mx-4 sm:w-[350px]">
             Join us for an unforgettable experience at Techember! Secure your
             spot now.
           </p>
-          <div className="font-roboto text-[16px] xs:flex-col xs:flex xs:items-center xs:justify-center sm:flex sm:flex-row xs:mt-4 sm:mt-0">
+          <div className="font-roboto text-[16px] flex-col flex items-center justify-center sm:flex sm:flex-row mt-4 sm:mt-0">
             <p>
               {' '}
               <i className="ri-map-pin-line mr-[3px]"></i> Oniru, Lekki
             </p>
             <div className="flex">
-              <p className="mx-[6px] xs:hidden sm:block">||</p>
+              <p className="mx-[6px] hidden sm:block">||</p>
               <p>March 15, 2025</p>
               <p className="mx-[6px]"> |</p>
               <p>7:00PM</p>
@@ -80,7 +79,7 @@ export default function TicketSelection({
           <option value="5">5</option>
         </select>
 
-        <div className=" xs:flex xs:flex-col-reverse sm:flex sm:flex-row items-center justify-between mb-8 w-full xs:gap-4">
+        <div className=" flex flex-col-reverse sm:flex sm:flex-row items-center justify-between mb-8 w-full gap-4">
           <button
             className="py-2 px-6 border border-[#24a0b5] text-[#24a0b5] rounded-lg xs:w-[100%] sm:w-[48%]  cursor-pointer outline-none"
             onClick={() => {
@@ -90,7 +89,7 @@ export default function TicketSelection({
             Cancel
           </button>
           <button
-            className="py-2 px-6 text-white bg-[#24a0b5] rounded-lg xs:w-[100%] sm:w-[48%] cursor-pointer outline-none"
+            className="py-2 px-6 text-white bg-[#24a0b5] rounded-lg w-[100%] sm:w-[48%] cursor-pointer outline-none"
             onClick={showAttendeeDetails}
           >
             Next
